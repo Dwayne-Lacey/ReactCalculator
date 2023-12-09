ac31679f1afab133bb53e6f6e738149eb80116e9
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2023-12-09 16:14:44 -0500

    Added logic to handle entry of operators during calc

--- my-app/src/features/calculator/Calculator.js
+++ my-app/src/features/calculator/Calculator.js
@@ -3,41 +3,67 @@ import { useSelector, useDispatch } from 'react-redux';
 import {
   setTotal,
   updateCalcString,
+  updateCalcValue,
   clearCalcString,
-  selectCalc
+  clearCalcValue,
+  selectCalc,
+  selectValue,
+  selectFunc
 } from './calcSlice';
 import styles from './Calculator.module.css';
 
 export function Calculator() {
   const calcString = useSelector(selectCalc);
+  const calcValue = useSelector(selectValue);
+  const calcFunc = useSelector(selectFunc);
   const dispatch = useDispatch();
-  // const addValue = (value) => {
-  //   //This should use the current calcString and perform string validation.
-  //   //If string is more than 10 characters, do nothing
-  //   //If string does not contain a period, only allow one 0
-  //   //If string does
-  //   dispatch(updateCalcString(value));
-  // }
+  const addValue = (value, func) => {
+    console.log("Current value = ", calcValue);
+    console.log("Current func bool = ", calcFunc);
+    console.log("New value = ", value);
+    console.log("New func = ", func);
+    if (func === calcFunc && func) {
+      //Adding func to value
+      //Get length of current value, if 0, then add func.
+      //Else if 1, if value is '-' then append the value
+      //else replace current value with value
+      if (calcValue.length === 1 && value === '-') {
+        dispatch(updateCalcValue({"value": value, "func": func}));
+      }
+      else {
+        dispatch(clearCalcValue());
+        dispatch(updateCalcValue({"value": value, "func": func}));
+      }
+    }
+    else if (func === calcFunc && !func) {
+      console.log("Not implemented yet");
+    }
+    else {
+      dispatch(updateCalcValue({"value": value, "func": func}));
+    }
+  }
 
   return (
     <div>
-      <h3></h3>
-      <h2></h2>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>0</button>
-      <button>X</button>
-      <button>-</button>
-      <button>+</button>
+      <h3>{calcFunc}</h3>
+      <h3>{calcString}</h3>
+      <h2>{calcValue}</h2>
+      <button onClick={() => {addValue("1", false)}}>1</button>
+      <button onClick={() => {addValue("2", false)}}>2</button>
+      <button onClick={() => {addValue("3", false)}}>3</button>
+      <button onClick={() => {addValue("4", false)}}>4</button>
+      <button onClick={() => {addValue("5", false)}}>5</button>
+      <button onClick={() => {addValue("6", false)}}>6</button>
+      <button onClick={() => {addValue("7", false)}}>7</button>
+      <button onClick={() => {addValue("8", false)}}>8</button>
+      <button onClick={() => {addValue("9", false)}}>9</button>
+      <button onClick={() => {addValue("0", false)}}>0</button>
+      <button onClick={() => {addValue(".", false)}}>.</button>
+      <button onClick={() => {addValue("X", true)}}>X</button>
+      <button onClick={() => {addValue("-", true)}}>-</button>
+      <button onClick={() => {addValue("+", true)}}>+</button>
+      <button onClick={() => {addValue("/", true)}}>/</button>
       <button>=</button>
-      <button>/</button>
       <button>AC</button>
     </div>
   );
--- my-app/src/features/calculator/calcSlice.js
+++ my-app/src/features/calculator/calcSlice.js
@@ -2,7 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 
 //Holds calculation
 const initialState = {
-  calcString: ""
+  calcString: "",
+  value: "",
+  func: false
 };
 
 export const calcSlice = createSlice({
@@ -13,20 +15,31 @@ export const calcSlice = createSlice({
     updateCalcString: (state, action) => {
       state.calcString += action.payload;
     },
+    updateCalcValue: (state, action) => {
+      state.value += action.payload.value;
+      state.func = action.payload.func;
+
+    },
     clearCalcString: (state) => {
       state.calcString = "";
     },
+    clearCalcValue: (state) => {
+      state.value = "";
+    },
     setTotal: (state, action) => {
-      state.calcString = action.payload;
+      state.calcString += action.payload;
+      state.value = "";
     }
   }
 });
 
-export const { updateCalcString, clearCalcString, setTotal } = calcSlice.actions;
+export const { updateCalcString, updateCalcValue, clearCalcString, clearCalcValue, setTotal } = calcSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectCalc = (state) => state.calculator.calcString;
+export const selectValue = (state) => state.calculator.value;
+export const selectFunc = (state) => state.calculator.func;
 
 export default calcSlice.reducer;
